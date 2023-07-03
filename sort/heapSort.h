2e8fb4215aab75ae9382a7e3863625e1abbd958f
tingsongs <1776272360@qq.com>
2023-07-03 13:09:57 +0800

    修改了堆排序算法，上一版本的算法不对

--- sort/heapSort.h
+++ sort/heapSort.h
@@ -3,18 +3,36 @@
 
 using namespace std;
 
+void adjust(vector<int>& heap, int len, int index)
+{
+    int left = 2*index+1, right = 2*index+2;
+
+    int maxId = index;
+
+    if(left < len && heap[len] > heap[maxId])
+        maxId = left;
+    if(right < len && heap[right] > heap[maxId])
+        maxId = right;
+    
+    if(maxId != index)
+    {
+        swap(heap[maxId], heap[index]);
+        adjust(heap, len, maxId);
+    }
+}
+
 void HeapSort(vector<int>& nums)
 {
-    for(int len = nums.size(); len > 1; len--)
+    int len = nums.size();
+    for(int n = len/2-1; n>=0; n--)
     {
-        for(int n = len/2-1; n >= 0; n--)
-        {
-            if(nums[n] < nums[2*n+1])
-                swap(nums[n], nums[2*n+1]);
-            if(2*n+2 < len && nums[n] < nums[2*n+2])
-                swap(nums[n], nums[2*n+2]);
-        }
+        adjust(nums, len, n);
+    }
+
+    while(len >= 2)
+    {
         swap(nums[0], nums[len-1]);
+        adjust(nums, --len, 0);
     }
 }
 
